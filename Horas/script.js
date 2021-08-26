a2f2fef6f132d4d3656509e7ac6ccc25be072401
jtthiago <69656413+jtthiago@users.noreply.github.com>
2021-08-26 12:00:19 -0300

    Add files via upload
    Verificador de hora otimizado

---
+++ Horas/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('img')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora =  data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'fotomanha (2).png'
+        document.body.style.background = '#F0E68C'
+    } else if (hora >= 12 && hora <18) {
+        //BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = '#FF8C00'
+    } else {
+        //BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#1C1C1C'
+    }
+
+}
